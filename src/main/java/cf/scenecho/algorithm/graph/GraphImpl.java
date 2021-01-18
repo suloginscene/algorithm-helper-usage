6e35610afb6262bed4413460020b808a3ec2017f
suloginscene <suloginscene@gmail.com>
2021-01-19 04:56:00 +0900

    feat: Implement graph

--- src/main/java/cf/scenecho/algorithm/graph/GraphImpl.java
+++ src/main/java/cf/scenecho/algorithm/graph/GraphImpl.java
@@ -1,56 +1,184 @@
 package cf.scenecho.algorithm.graph;
 
+import com.github.suloginscene.algorithmhelper.core.graph.Alphabet;
 import com.github.suloginscene.algorithmhelper.core.graph.Course;
 import com.github.suloginscene.algorithmhelper.core.graph.Graph;
 import com.github.suloginscene.algorithmhelper.core.graph.Path;
+import com.github.suloginscene.algorithmhelper.core.graph.SetSet;
 import lombok.NonNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 
 
-public class GraphImpl extends Graph<Integer> {
+public class GraphImpl extends Graph<Alphabet> {
 
-    private final Map<Integer, Set<Integer>> vertexMap = new HashMap<>();
-    private final Map<Path<Integer>, Path<Integer>> pathMap = new HashMap<>();
+    private final Map<Alphabet, Set<Alphabet>> vertexMap = new HashMap<>();
+    private final Map<Path<Alphabet>, Path<Alphabet>> pathMap = new HashMap<>();
 
 
     @Override
-    protected Map<Integer, Set<Integer>> getVertexMap() {
+    protected Map<Alphabet, Set<Alphabet>> getVertexMap() {
         return vertexMap;
     }
 
     @Override
-    protected Map<Path<Integer>, Path<Integer>> getPathMap() {
+    protected Map<Path<Alphabet>, Path<Alphabet>> getPathMap() {
         return pathMap;
     }
 
 
     @Override
-    public void bfs(@NonNull Integer start, Consumer<Integer> consumer) {
+    public void bfs(@NonNull Alphabet start, Consumer<Alphabet> consumer) {
+        Set<Alphabet> visited = new HashSet<>();
 
+        visited.add(start);
+        consumer.accept(start);
+
+        Set<Alphabet> neighbors = vertexMap.get(start);
+        Queue<Alphabet> queue = new LinkedList<>(neighbors);
+
+        while (!queue.isEmpty()) {
+            Alphabet neighbor = queue.poll();
+            visited.add(neighbor);
+            consumer.accept(neighbor);
+            if (vertexMap.containsKey(neighbor)) {
+                for (Alphabet subNeighbor : vertexMap.get(neighbor)) {
+                    if (!visited.contains(subNeighbor) && !queue.contains(subNeighbor)) {
+                        queue.add(subNeighbor);
+                    }
+                }
+            }
+        }
     }
 
     @Override
-    public void dfs(@NonNull Integer start, Consumer<Integer> consumer) {
+    public void dfs(@NonNull Alphabet start, Consumer<Alphabet> consumer) {
+        Set<Alphabet> visited = new HashSet<>();
+        recursiveDfs(start, consumer, visited);
+    }
 
+    private void recursiveDfs(Alphabet vertex, Consumer<Alphabet> consumer, Set<Alphabet> visited) {
+        if (visited.contains(vertex)) return;
+
+        visited.add(vertex);
+        consumer.accept(vertex);
+
+        if (vertexMap.containsKey(vertex)) {
+            for (Alphabet neighbor : vertexMap.get(vertex)) {
+                recursiveDfs(neighbor, consumer, visited);
+            }
+        }
     }
 
     @Override
-    protected void doTopologicalSort(Consumer<Integer> consumer) {
+    protected void doTopologicalSort(Consumer<Alphabet> consumer) {
+        Set<Alphabet> visited = new HashSet<>();
+        Stack<Alphabet> stack = new Stack<>();
 
+        for (Alphabet key : vertexMap.keySet()) {
+            if (!visited.contains(key)) {
+                stackDfs(key, visited, stack);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            consumer.accept(stack.pop());
+        }
     }
 
-    @Override
-    public Map<Integer, Course<Integer>> shortestCourses(@NonNull Integer start) {
-        return null;
+    private void stackDfs(Alphabet vertex, Set<Alphabet> visited, Stack<Alphabet> stack) {
+        if (visited.contains(vertex)) return;
+
+        visited.add(vertex);
+        if (vertexMap.containsKey(vertex)) {
+            for (Alphabet neighbor : vertexMap.get(vertex)) {
+                stackDfs(neighbor, visited, stack);
+            }
+        }
+        stack.push(vertex);
     }
 
+
     @Override
-    public Graph<Integer> minimumSpanningTree() {
-        return null;
+    protected Map<Alphabet, Course<Alphabet>> doShortestCourses(@NonNull Alphabet start) {
+        Set<Alphabet> vertices = allVertices();
+
+        Map<Alphabet, Integer> distances = new HashMap<>();
+        Map<Alphabet, Alphabet> predecessor = new HashMap<>();
+        for (Alphabet vertex : vertices) {
+            distances.put(vertex, Integer.MAX_VALUE);
+            predecessor.put(vertex, null);
+        }
+        distances.put(start, 0);
+
+        Set<Alphabet> visited = new HashSet<>();
+        while (visited.size() < vertices.size()) {
+            Alphabet current = null;
+            int currDist = Integer.MAX_VALUE;
+            for (Map.Entry<Alphabet, Integer> e : distances.entrySet()) {
+                Alphabet tempVertex = e.getKey();
+                Integer tempDist = e.getValue();
+                if (!visited.contains(tempVertex) && (tempDist < currDist)) {
+                    current = tempVertex;
+                    currDist = tempDist;
+                }
+            }
+            visited.add(current);
+            if (vertexMap.containsKey(current)) {
+                for (Alphabet neighbor : vertexMap.get(current)) {
+                    int knownDist = distances.get(neighbor);
+                    int altDist = currDist + pathMap.get(new Path<>(current, neighbor, 0)).getWeight();
+                    if (altDist < knownDist) {
+                        distances.put(neighbor, altDist);
+                        predecessor.put(neighbor, current);
+                    }
+                }
+            }
+        }
+
+        Map<Alphabet, Course<Alphabet>> courses = new HashMap<>();
+        for (Alphabet dest : vertices) {
+            Stack<Alphabet> stack = new Stack<>();
+            Alphabet ptr = dest;
+            while (ptr != null) {
+                stack.add(ptr);
+                ptr = predecessor.get(ptr);
+            }
+            List<Alphabet> list = new ArrayList<>();
+            while (!stack.isEmpty()) {
+                list.add(stack.pop());
+            }
+            courses.put(dest, new Course<>(list, distances.get(dest)));
+        }
+        return courses;
+    }
+
+
+    @Override
+    protected Graph<Alphabet> doMinimumSpanningTree() {
+        SetSet<Alphabet> sets = new SetSet<>();
+        for (Alphabet vertex : allVertices()) {
+            Set<Alphabet> set = new HashSet<>();
+            set.add(vertex);
+            sets.add(set);
+        }
+
+        PriorityQueue<Path<Alphabet>> pq = new PriorityQueue<>(pathMap.keySet());
+
+        GraphImpl graph = new GraphImpl();
+        while (sets.size() > 1 && !pq.isEmpty()) {
+            Path<Alphabet> shortest = pq.poll();
+            Alphabet from = shortest.getFrom();
+            Alphabet to = shortest.getTo();
+            Set<Alphabet> setA = sets.findSet(from);
+            Set<Alphabet> setB = sets.findSet(to);
+            if (setA != setB) {
+                graph.addEdge(from, to, shortest.getWeight());
+                sets.union(setA, setB);
+            }
+        }
+        return graph;
     }
 
 }
--- src/test/java/cf/scenecho/algorithm/graph/GraphImplTest.java
+++ src/test/java/cf/scenecho/algorithm/graph/GraphImplTest.java
@@ -1,18 +1,47 @@
 package cf.scenecho.algorithm.graph;
 
+import com.github.suloginscene.algorithmhelper.core.graph.Alphabet;
+import com.github.suloginscene.algorithmhelper.core.graph.Course;
 import com.github.suloginscene.algorithmhelper.core.graph.Graph;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.github.suloginscene.algorithmhelper.core.graph.Alphabet.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 class GraphImplTest {
 
-    Graph<Integer> graph;
+    Graph<Alphabet> graph;
+
 
     @BeforeEach
     void setup() {
         graph = new GraphImpl();
+    }
+
+    void initUnWeighted() {
+        graph.addEdge(A, B, 0);
+        graph.addEdge(B, C, 0);
+        graph.addEdge(C, D, 0);
+        graph.addEdge(D, E, 0);
+
+        graph.addEdge(A, D, 0);
+    }
+
+    void initWeighted() {
+        graph.addEdge(A, B, 1);
+        graph.addEdge(B, C, 2);
+        graph.addEdge(C, D, 3);
+        graph.addEdge(D, E, 4);
+
+        graph.addEdge(A, D, 5);
     }
 
     @AfterEach
@@ -22,7 +51,54 @@ class GraphImplTest {
 
 
     @Test
-    void test() {
+    void bfs() {
+        initUnWeighted();
+
+        List<Alphabet> result = new ArrayList<>();
+        graph.bfs(A, result::add);
+
+        System.out.println(result);
+    }
+
+    @Test
+    void dfs() {
+        initUnWeighted();
+
+        List<Alphabet> result = new ArrayList<>();
+        graph.dfs(A, result::add);
+
+        System.out.println(result);
+    }
+
+    @Test
+    void topologicalSort() {
+        initUnWeighted();
+
+        List<Alphabet> result = new ArrayList<>();
+        graph.topologicalSort(result::add);
+
+        System.out.println(result);
+    }
+
+    @Test
+    void shortest() {
+        initWeighted();
+
+        Map<Alphabet, Course<Alphabet>> courses = graph.shortestCourses(A);
+        Course<Alphabet> toE = courses.get(E);
+        toE.print();
+
+        assertEquals(9, toE.getDistance());
+    }
+
+    @Test
+    void mst() {
+        initWeighted();
+
+        graph = graph.minimumSpanningTree();
+
+        assertFalse(graph.hasCycle());
+        assertEquals(graph.allVertices().size() - 1, graph.allPaths().size());
     }
 
 }
